b07bbc0583a658c314fdca32bdcee71a766ae35e
Ayodeji Adesola <82943610+deji12@users.noreply.github.com>
2022-10-23 22:18:09 +0800

    allowed host update

--- movie/settings.py
+++ movie/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-2ns(12mi_0-i^lq8z9&*$u90lqt9#x6k*z(*o-kl*izfq7yl)y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['https://new-movie-production.up.railway.app/', '*']
 
 # SECURE_SSL_REDIRECT = True
 # Application definition
