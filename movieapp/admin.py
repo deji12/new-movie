4f8bf79d4e60d13a2e5d75627f6e71d4a296492d
Ayodeji Adesola <82943610+deji12@users.noreply.github.com>
2022-11-13 19:36:44 +0800

    adding import export to models

--- movieapp/admin.py
+++ movieapp/admin.py
@@ -1,7 +1,74 @@
 from django.contrib import admin
 from . import models
 
-# Register your models here.
+from import_export import resources
+
+class MovieResource(resources.ModelResource):
+
+    class Meta:
+        model = models.movie
+        
+class commentResource(resources.ModelResource):
+
+    class Meta:
+        model = models.comment
+        
+class ReviewsResource(resources.ModelResource):
+
+    class Meta:
+        model = models.reviewss
+        
+class CategoryResource(resources.ModelResource):
+
+    class Meta:
+        model = models.Category
+        
+class RateResource(resources.ModelResource):
+
+    class Meta:
+        model = models.rate
+        
+class YearResource(resources.ModelResource):
+
+    class Meta:
+        model = models.year
+        
+class SeriesResource(resources.ModelResource):
+
+    class Meta:
+        model = models.series
+        
+class EpisodeCommentResource(resources.ModelResource):
+
+    class Meta:
+        model = models.episode_comment
+        
+class EpisodeReviewResource(resources.ModelResource):
+
+    class Meta:
+        model = models.episode_review
+        
+class SeasonResource(resources.ModelResource):
+
+    class Meta:
+        model = models.season
+        
+class EpisodeResource(resources.ModelResource):
+
+    class Meta:
+        model = models.episode
+        
+class PhotoResource(resources.ModelResource):
+
+    class Meta:
+        model = models.photos
+        
+        
+class ProfileResource(resources.ModelResource):
+
+    class Meta:
+        model = models.Profile
+        
 admin.site.register(models.movie)
 admin.site.register(models.comment)
 admin.site.register(models.reviewss)
